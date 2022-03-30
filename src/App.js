b1e672b926c715941bf78e8ee78cce61dedf9e15
Andreu Slynchyk <andreu.slynchyk@gmail.com>
2022-03-30 18:15:32 +0300

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Lauout from "./frontPages/hoc/Lauout";
 import Header from "./frontPages/hoc/Header";
 import styles from './frontPages/Styled/styles.css'
+import Home from "./frontPages/Pages/Home";
+import TimeLine from "./frontPages/Pages/TimeLine";
 
 function App() {
     return (
@@ -9,7 +12,16 @@ function App() {
 
             </Header>
             <Lauout>
-
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={
+                            <Home/>
+                        }/>
+                        <Route path='/Timeline' element={
+                            <TimeLine/>
+                        }/>
+                    </Routes>
+                </BrowserRouter>
             </Lauout>
         </>
     );
